d6c3764de4237ad8f5c4be37bda5483e33c2a4e4
JandreiSS <jandreisst@gmail.com>
2020-07-01 11:08:23 -0300

    chore: Refactoring structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <h1>GoBarber</h1>
-  )
-}
+const App: React.FC = () => <h1>GoBarber</h1>
 
 export default App;
