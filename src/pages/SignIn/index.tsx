476aadf579ca4ce6d50d89b095275fc322956ac6
JandreiSS <jandreisst@gmail.com>
2020-07-20 15:19:19 -0300

    chore: Context login, using local storage and create useAuth hook

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,10 @@
-import React, { useRef, useCallback, useContext } from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
-import AuthContext from '../../context/AuthContext';
+import { useAuth } from '../../context/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
@@ -14,64 +14,75 @@ import Button from '../../components/Button';
 
 import { Container, Content, Background } from './styles';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
 
-  const { name } = useContext(AuthContext);
+  const { signIn } = useAuth();
 
-  const handleSubmit = useCallback(async (data: object) => {
-    try {
-      formRef.current?.setErrors({});
+  const handleSubmit = useCallback(
+    async (data: SignInFormData) => {
+      try {
+        formRef.current?.setErrors({});
 
-      const schema = Yup.object().shape({
-        email: Yup.string()
+        const schema = Yup.object().shape({
+          email: Yup.string()
           .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
-        password: Yup.string()
+          password: Yup.string()
           .required('Senha obrigatória'),
-      });
+        });
 
-      await schema.validate(data, {
-        abortEarly: false,
-      });
-    } catch (err) {
-      const errors = getValidationErrors(err);
+        await schema.validate(data, {
+          abortEarly: false,
+        });
 
-      formRef.current?.setErrors(errors);
-    }
-  }, []);
+        signIn({
+          email: data.email,
+          password: data.password,
+        });
+      } catch (err) {
+        const errors = getValidationErrors(err);
 
-  return (
-    <Container>
+        formRef.current?.setErrors(errors);
+      }
+    }, [signIn]);
+
+    return (
+      <Container>
       <Content>
-        <img src={logoImg} alt="GoBarber" />
+      <img src={logoImg} alt="GoBarber" />
 
-        <Form ref={formRef} onSubmit={handleSubmit}>
-          <h1>Faça seu logon</h1>
+      <Form ref={formRef} onSubmit={handleSubmit}>
+      <h1>Faça seu logon</h1>
 
-          <Input name="email" icon={FiMail} placeholder="E-mail" />
+      <Input name="email" icon={FiMail} placeholder="E-mail" />
 
-          <Input
-            name="password"
-            icon={FiLock}
-            type="password"
-            placeholder="Senha"
-          />
+      <Input
+      name="password"
+      icon={FiLock}
+      type="password"
+      placeholder="Senha"
+      />
 
-          <Button type="submit">Entrar</Button>
+      <Button type="submit">Entrar</Button>
 
-          <a href="forgot">Esqueci minha senha</a>
-        </Form>
+      <a href="forgot">Esqueci minha senha</a>
+      </Form>
 
-        <a href="login">
-          <FiLogIn />
-          Criar conta
-        </a>
+      <a href="login">
+      <FiLogIn />
+      Criar conta
+      </a>
       </Content>
 
       <Background />
-    </Container>
-  );
-};
+      </Container>
+      );
+    };
 
-export default SignIn;
+    export default SignIn;
