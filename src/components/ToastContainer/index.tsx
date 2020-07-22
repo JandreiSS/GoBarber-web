4278f171f507cae20bd9c524f3eb547db7f35126
JandreiSS <jandreisst@gmail.com>
2020-07-22 15:43:09 -0300

    chore: Configuring routes

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -4,7 +4,7 @@ import { useTransition } from 'react-spring';
 import Toast from './Toast';
 
 import { Container } from './styles';
-import { ToastMessage, useToast } from '../../hooks/toast';
+import { ToastMessage } from '../../hooks/toast';
 
 interface ToastContainerProps {
   messages: ToastMessage[];
